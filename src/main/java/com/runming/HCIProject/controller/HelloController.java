e4988683d5125b319b07ef600442907e4d223c47
zzt96 <136901341@qq.com>
2020-11-21 15:46:07 -0500

    init

---
+++ src/main/java/com/runming/HCIProject/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.runming.HCIProject.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @RequestMapping("/")
+    public String SayHello() {
+        return "hello world";
+    }
+}
