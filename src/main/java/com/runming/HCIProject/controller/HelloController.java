859ec58f96e5379a16c1d44e1c9d95515f0b258e
zzt96 <136901341@qq.com>
2020-11-23 20:00:46 -0500

    cors

--- src/main/java/com/runming/HCIProject/controller/HelloController.java
+++ src/main/java/com/runming/HCIProject/controller/HelloController.java
@@ -1,9 +1,11 @@
 package com.runming.HCIProject.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/hello")
 public class HelloController {
 
